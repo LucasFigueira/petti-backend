6c36832ba89f3bd9e2bb2fc0aab849f9a1823ba4
Lucas Figueira <lucas.figueira@petlove.com.br>
2021-06-04 13:56:30 -0300

    :tada: petti project

---
+++ src/main/java/br/com/petti/PettiApplication.java
@@ -0,0 +1,13 @@
+package br.com.petti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petti/PettiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
