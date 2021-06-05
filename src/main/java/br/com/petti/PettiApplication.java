0a2d328206180eb474ad10874f7b32f9a3bea89f
LucasFigueira <Lfgon7@gmail.com>
2021-06-04 23:16:14 -0300

    :recycle: configure .war app

--- src/main/java/br/com/petti/PettiApplication.java
+++ src/main/java/br/com/petti/PettiApplication.java
@@ -2,12 +2,19 @@ package br.com.petti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PettiApplication {
+public class PettiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PettiApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PettiApplication.class);
 	}
 
 }
